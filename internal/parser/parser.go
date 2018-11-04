c8840663567f09d2d0e3fceb60765a40158c0ce5
Jonathan A. Sternberg <jonathan@influxdata.com>
2018-11-04 10:18:25 -0600

    simple parser implementation
    This is an idea for how to lay the groundwork for a parser that will
    also be useful for both code completion and better error messages
    (hopefully).
    
    This is a problem I ran into frequently when trying to use influxql
    outside of the actual database. Interactive use meant that the parser
    itself wasn't interruptible. This parser is built in a way where it can
    partially consume an input and then return a checkpoint for where it is
    currently located. Normally, this would require the stack and it
    wouldn't be possible to continue without starting a goroutine and
    running the parser within that goroutine.
    
    This also may make it possible to build the repl in a way where it will
    automatically execute commands, but if it sees something like `|>` at
    the end, it will see the statement is incomplete and that it should
    continue waiting for the parser to be sent more input.

---
+++ internal/parser/parser.go
@@ -0,0 +1,296 @@
+// Package parser implements a parser for flux.
+//
+// The parser is composed of a series of ParseNode objects. The ParseNode has a simple interface.
+// It accepts the Scanner which is defined within this package so that alternative scanning
+// implementations can be swapped out and different ones can be experimented with.
+//
+// Each ParseNode takes the Scanner as input. If a ParseNode is able to do something with the next
+// token, it will consume the token and produce a new ParseNode that will move to the next stage
+// of parsing. For efficiency, a ParseNode may consume more than one token.
+//
+// Once a ParseNode is not able to consume any more input from the Scanner, it indicates that the
+// parsing is in one of two states: terminal or non-terminal. A terminal state means that no more
+// input will be accepted no matter what gets fed into the parser. This is most commonly used with
+// error states, but it may also indicate that a subtree in the parse tree has finished. A
+// non-terminal state means that the system could continue feeding input to the node.
+//
+// After reaching either a terminal or non-terminal state, the code calling the parser can invoke
+// the Get method on the ParseNode. The Get method will construct the ast.Node if it is able to
+// and it will return an error if the parser ended in a terminal error state.
+//
+// The state within each ParseNode is immutable after it has been returned. That means that each stage
+// of the parsing could be retained and the state of the Scanner could be preserved for each one
+// to be able to replay the entire process. While the state is immutable, the ParseNode's are not
+// thread or memory-safe. If you use an older ParseNode, it may use the same memory as a different
+// ParseNode for things involving slices and maps. The returned nodes are shallow copies.
+//
+// TODO(jsternberg): I need to find a way to report the difference between a non-final state error
+// and an error-error. Most likely through a custom error type that will both signal that the error
+// is recoverable and indicate ways to correct it (such as through completion suggestions maybe?).
+package parser
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/influxdata/flux/ast"
+	"github.com/influxdata/flux/internal/token"
+	"github.com/pkg/errors"
+)
+
+// Scanner defines the interface for reading a stream of tokens.
+type Scanner interface {
+	// Scan will scan the next token.
+	Scan() (token.Pos, token.Token, string)
+
+	// ScanNoRegex will scan the next token, but exclude any
+	// regex literals.
+	ScanNoRegex() (token.Pos, token.Token, string)
+
+	// Unread will unread back to the previous location within the Scanner.
+	// This can only be called once so the maximum lookahead is one.
+	Unread()
+}
+
+// ParseNode refers to a portion of the parse tree. It holds the current location
+// within the parser so that the parser can be interrupted and continued
+// as the controlling program sees is needed. It may also contain information about
+// what tokens are expected and which tokens have been encountered to aid with
+// syntax highlighting and code completion.
+type ParseNode interface {
+	// Parse takes the Scanner. Parse will always return a ParseNode. If the ParseNode
+	// was able to successfully consume at least one token from the Scanner, then the
+	// second argument will be true. If the Scanner could not be advanced, the second
+	// argument will be false and the ParseNode will either contain a valid AST fragment,
+	// a partial AST fragment, or it will return an error.
+	Parse(s Scanner) (ParseNode, bool)
+
+	// Get will retrieve the AST node that has been constructed from the ParseNode.
+	Get() (ast.Node, error)
+}
+
+// TerminalNode is a (potentially) terminal ParseNode.
+type TerminalNode interface {
+	ParseNode
+
+	// IsTerminal indicates if this is a terminal node.
+	IsTerminal() bool
+}
+
+// IsTerminal will determine if a ParseNode is terminal. If the ParseNode does not
+// implement the TerminalNode interface, it is assumed to be a non-terminal node.
+func IsTerminal(n ParseNode) bool {
+	if n, ok := n.(TerminalNode); ok {
+		return n.IsTerminal()
+	}
+	return false
+}
+
+// Feed will continuous feed the Scanner to the ParseNode until Parse returns false.
+// It will then return the last ParseNode in the series.
+func Feed(n ParseNode, s Scanner) ParseNode {
+	for {
+		var ok bool
+		n, ok = n.Parse(s)
+		if !ok {
+			return n
+		}
+	}
+}
+
+func New() ParseNode {
+	return Program{}
+}
+
+type Program struct {
+	root ast.Program
+	stmt ParseNode
+}
+
+func (p Program) Parse(s Scanner) (ParseNode, bool) {
+	if p.stmt != nil {
+		stmt, ok := p.stmt.Parse(s)
+		if ok {
+			p.stmt = stmt
+			return p, true
+		}
+		// Delay generating the statement temporarily.
+		// If we get an EOF, we want to keep the current
+		// state before we materialize the object. If
+		// we don't get an EOF, then we want to attempt to
+		// materialize the statement and add it to the program.
+	}
+
+	// Read the next token.
+	_, tok, lit := s.Scan()
+	if tok != token.EOF && p.stmt != nil {
+		// Materialize the previous statement. If we cannot because
+		// of an error, then we cannot advance the node and we need
+		// to unread the token.
+		stmt, err := p.stmt.Get()
+		if err != nil {
+			s.Unread()
+			return p, false
+		}
+
+		p.root.Body = append(p.root.Body, stmt.(ast.Statement))
+		p.stmt = nil
+	}
+
+	switch tok {
+	case token.IDENT:
+		return p.Ident(lit), true
+	case token.EOF:
+		return p, false
+	default:
+		s.Unread()
+		return Errorf("unexpected token: %d", tok), false
+	}
+}
+
+func (p Program) Get() (ast.Node, error) {
+	if p.stmt != nil {
+		stmt, err := p.stmt.Get()
+		if err != nil {
+			return nil, err
+		}
+
+		stmts := make([]ast.Statement, len(p.root.Body), len(p.root.Body)+1)
+		copy(stmts, p.root.Body)
+		p.root.Body = append(p.root.Body, stmt.(ast.Statement))
+	}
+	return &p.root, nil
+}
+
+func (p Program) Ident(name string) ParseNode {
+	return parseFunc{
+		ParseFn: func(s Scanner) (ParseNode, bool) {
+			// We are either expecting an assignment or we are in an expression statement.
+			switch _, tok, _ := s.Scan(); tok {
+			case token.ASSIGN:
+				return p.Assignment(name), true
+			default:
+				// The others are not implemented yet.
+				s.Unread()
+				return Errorf("implement me"), false
+			}
+		},
+		GetFn: func() (ast.Node, error) {
+			return nil, fmt.Errorf("unexpected")
+		},
+	}
+}
+
+func (p Program) Assignment(name string) ParseNode {
+	p.stmt = VariableDeclaration{
+		name: name,
+		expr: UnaryExpr{},
+	}
+	return p
+}
+
+type VariableDeclaration struct {
+	name string
+	expr ParseNode
+}
+
+func (vd VariableDeclaration) Parse(s Scanner) (ParseNode, bool) {
+	var ok bool
+	vd.expr, ok = vd.expr.Parse(s)
+	return vd, ok
+}
+
+func (vd VariableDeclaration) Get() (ast.Node, error) {
+	expr, err := vd.expr.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	return &ast.VariableDeclaration{
+		Declarations: []*ast.VariableDeclarator{
+			{
+				ID:   &ast.Identifier{Name: vd.name},
+				Init: expr.(ast.Expression),
+			},
+		},
+	}, nil
+}
+
+type errorNode struct {
+	Err error
+}
+
+func (e errorNode) Parse(s Scanner) (ParseNode, bool) {
+	return e, false
+}
+
+func (e errorNode) Get() (ast.Node, error) {
+	return nil, e.Err
+}
+
+func (e errorNode) IsTerminal() bool {
+	return true
+}
+
+func Error(err error) ParseNode {
+	return errorNode{Err: err}
+}
+
+func Errorf(msg string, v ...interface{}) ParseNode {
+	return Error(fmt.Errorf(msg, v...))
+}
+
+type parseFunc struct {
+	ParseFn func(s Scanner) (ParseNode, bool)
+	GetFn   func() (ast.Node, error)
+}
+
+func (fn parseFunc) Parse(s Scanner) (ParseNode, bool) {
+	return fn.ParseFn(s)
+}
+
+func (fn parseFunc) Get() (ast.Node, error) {
+	return fn.GetFn()
+}
+
+type UnaryExpr struct {
+	expr ast.Expression
+}
+
+func ParseExpr(s Scanner) ParseNode {
+	return UnaryExpr{}
+}
+
+func (e UnaryExpr) Parse(s Scanner) (ParseNode, bool) {
+	if e.expr != nil {
+		// TODO(jsternberg): Attempt to read an operator.
+		return e, false
+	}
+
+	_, tok, lit := s.Scan()
+	expr, err := func() (ast.Expression, error) {
+		switch tok {
+		case token.STRING:
+			s, err := strconv.Unquote(lit)
+			if err != nil {
+				return nil, errors.Wrap(err, "string literal must be surrounded by quotes")
+			}
+			return &ast.StringLiteral{Value: s}, nil
+		default:
+			return nil, fmt.Errorf("unexpected token: %d", tok)
+		}
+	}()
+	if err != nil {
+		s.Unread()
+		return Error(err), false
+	}
+	e.expr = expr
+	return e, true
+}
+
+func (e UnaryExpr) Get() (ast.Node, error) {
+	if e.expr == nil {
+		return nil, fmt.Errorf("incomplete")
+	}
+	return e.expr, nil
+}
---
+++ internal/parser/parser_test.go
@@ -0,0 +1,93 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/influxdata/flux/ast"
+	"github.com/influxdata/flux/internal/parser"
+	"github.com/influxdata/flux/internal/token"
+)
+
+type Token struct {
+	Pos   token.Pos
+	Token token.Token
+	Lit   string
+}
+
+type Scanner struct {
+	Tokens   []Token
+	i        int
+	buffered bool
+}
+
+func (s *Scanner) Scan() (token.Pos, token.Token, string) {
+	if s.i >= len(s.Tokens) {
+		return 0, token.EOF, ""
+	}
+	tok := s.Tokens[s.i]
+	s.i++
+	s.buffered = false
+	return tok.Pos, tok.Token, tok.Lit
+}
+
+func (s *Scanner) ScanNoRegex() (token.Pos, token.Token, string) {
+	pos, tok, lit := s.Scan()
+	if tok == token.REGEX {
+		// The parser was asking for a non regex token and our static
+		// scanner gave it one. This indicates a bug in the parser since
+		// the parser should have invoked Scan instead.
+		s.Unread()
+		return 0, token.ILLEGAL, ""
+	}
+	return pos, tok, lit
+}
+
+func (s *Scanner) Unread() {
+	// Buffered indicates that the value is "buffered". Since we keep everything
+	// in memory, we use it to prevent unread from going backwards more than once
+	// to prevent accidentally using a lookahead of 2 when writing the parser.
+	if !s.buffered {
+		s.buffered = true
+		s.i--
+	}
+}
+
+func TestParser(t *testing.T) {
+	for _, tt := range []struct {
+		Name   string
+		Tokens []Token
+		Result *ast.Program
+	}{
+		{
+			Name: "variable declaration",
+			Tokens: []Token{
+				{Token: token.IDENT, Lit: "a"},
+				{Token: token.ASSIGN, Lit: "="},
+				{Token: token.STRING, Lit: `"hello"`},
+			},
+			Result: &ast.Program{
+				Body: []ast.Statement{
+					&ast.VariableDeclaration{
+						Declarations: []*ast.VariableDeclarator{
+							{ID: &ast.Identifier{Name: "a"}, Init: &ast.StringLiteral{Value: "hello"}},
+						},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tt.Name, func(t *testing.T) {
+			scanner := &Scanner{Tokens: tt.Tokens}
+			n := parser.Feed(parser.New(), scanner)
+			result, err := n.Get()
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if got, want := result, tt.Result; !cmp.Equal(want, got) {
+				t.Fatalf("unexpected statement -want/+got\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
